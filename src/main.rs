f0474f2e60b4e57c79b1f4b3b208abf3290fa95e
Ttst <thibautstachnick@gmail.com>
2023-12-25 12:04:12 +0100

    add first exercise

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    exo_1()
 }
+
+
+
+use std::collections::HashMap;
+fn exo_1(){
+    let mut list = vec![31,83,23,90,263];
+
+    list.sort();
+
+    println!("La médiane de la liste triée est {:?}", list[2]);
+
+    let mut hashed_list = HashMap::new();
+
+    for element in list {
+        let counter = hashed_list.entry(element).or_insert(0);
+        *counter +=1
+    }
+
+    for (key,value) in hashed_list {
+        println!("La clé {} a pour valeur {}",key,value)
+    }
+
+}
+
